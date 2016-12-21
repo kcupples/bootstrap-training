0adf4e326908b4a3266f348ec60b6092a9b31fee
Cupples <kcupples@acumensolutions.com>
2016-12-21 15:58:36 -0500

    moved to public folder

--- main.js
+++ main.js
@@ -1,28 +1,13 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var express = require('express');
+var app = express();
 
-//Create the server
-http.createServer(function (request, response){
-	//Parse the URL for the file name
-	var pathName=url.parse(request.url).pathname;
-	console.log('Request for: ' + pathName);
+app.get('/', function (req, res) {
+   
+})
 
-	fs.readFile(pathName.substr(1), function (err,data){
-		if (err){
-			console.log(err);
-			//Set HTTP Status to 404
-			//Set Content Type to text/plain
-			response.writeHead(404, {'Content-Type': 'text/html'});
-		}
-		else {
-			//Page Found
-			//Set HTTP Status to 200
-			response.writeHead(200, {'Content-Type' : 'text/html'});
-			response.write(data.toString());
-		}
-		response.end();
-
-		});
-}).listen(80);
-console.log('Server running on port 8082');
+var server = app.listen(8082, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
