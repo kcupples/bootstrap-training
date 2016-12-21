f81be76f31571af6078fd7fd1e479e9c7aed6415
Cupples <kcupples@acumensolutions.com>
2016-12-21 15:31:02 -0500

    added web server stuff

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-/*Hello World Program - Node.js */
-var http=require("http");
-var express=require("express");
-http.createServer (function (request, response){
-	//Send HTTP Header
-	//HTTP Status=200 : OK
-	//Content Type: text/plain
-	response.writeHead(200, {'Content-Type': 'text/plain'});
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-	//Send Response in Body as "Hello World"
-	response.end('Hello World\n');
-}).listen(8082);
+//Create the server
+http.createServer(function (request, response){
+	//Parse the URL for the file name
+	var pathName=url.parse(request.url).pathname;
+	console.log('Request for: ' + pathName);
 
-console.log("Http Server running on 8082");
+	fs.readFile(pathName.substr(1), function (err,data){
+		if (err){
+			console.log(err);
+			//Set HTTP Status to 404
+			//Set Content Type to text/plain
+			response.writeHead(404, {'Content-Type': 'text/html'});
+		}
+		else {
+			//Page Found
+			//Set HTTP Status to 200
+			response.writeHead(200, {'Content-Type' : 'text/html'});
+			response.write(data.toString());
+		}
+		response.end();
+
+		});
+}).listen(80);
+console.log('Server running on port 8082');
